0c564c295e834fbfb21e77feb3e970ce4941df6c
afif_pathan <afif.pathan52@gmail.com>
2019-09-19 17:43:25 +0530

    First Commit

--- lib/home/ListStoredData.dart
+++ lib/home/ListStoredData.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/home/AddDataInList.dart';
 import 'package:flutter_app/home/Utils.dart';
-import 'package:flutter_app/main.dart';
 
 import 'AddItemInList.dart';
 
@@ -21,19 +19,14 @@ class DyanmicList extends State<ListStoredData> {
             IconButton(
                 icon: Icon(Icons.add_circle),
                 color: Colors.white,
+                tooltip: "test",
                 iconSize: 30.0,
                 onPressed: () {
-
                   showDialog(
                       context: context,
                       builder: (context) {
                         return AddItemInList();
                       });
-
-                  /*Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AddItemInList()),
-                  );*/
                 }),
           ],
         ),
@@ -41,82 +34,85 @@ class DyanmicList extends State<ListStoredData> {
           children: <Widget>[
             Expanded(
                 child: Align(
-                  alignment: Alignment.topCenter,
-                  child: ListView.builder(
-                      reverse: true,
-                      shrinkWrap: true,
-                      dragStartBehavior: DragStartBehavior.start,
-                      itemCount: Utils.usersUtils.length,
-                      itemBuilder: (BuildContext ctxt, int position) {
-                        if (Utils.usersUtils == null) {
-                          return Center(child: Text("No Data"));
-                        } else {
-                          return GestureDetector(
-                            onTap: () {},
-                            child: Column(
-                              children: <Widget>[
-                                Row(
+              alignment: Alignment.topCenter,
+              child: ListView.builder(
+                  reverse: true,
+                  //ForReverseOrder
+                  shrinkWrap: true,
+                  //ForReverseOrder
+                  dragStartBehavior: DragStartBehavior.start,
+                  //ForReverseOrder
+                  itemCount: Utils.usersUtils.length,
+                  itemBuilder: (BuildContext ctxt, int position) {
+                    if (Utils.usersUtils == null) {
+                      return Center(
+                          child: Text("No Data",
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  fontSize: 30.0, color: Colors.green)));
+                    } else {
+                      return Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        12.0, 12.0, 12.0, 6.0),
+                                    child: Text(
+                                      Utils.usersUtils[position].name,
+                                      style: TextStyle(
+                                          fontSize: 22.0,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        12.0, 6.0, 12.0, 12.0),
+                                    child: Text(
+                                      Utils.usersUtils[position].address,
+                                      style: TextStyle(fontSize: 18.0),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
                                   mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                                      MainAxisAlignment.spaceEvenly,
                                   children: <Widget>[
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              12.0, 12.0, 12.0, 6.0),
-                                          child: Text(
-                                            Utils.usersUtils[position].name,
-                                            style: TextStyle(
-                                                fontSize: 22.0,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              12.0, 6.0, 12.0, 12.0),
-                                          child: Text(
-                                            Utils.usersUtils[position].address,
-                                            style: TextStyle(fontSize: 18.0),
-                                          ),
-                                        ),
-                                      ],
+                                    Text(
+                                      "5m",
+                                      style: TextStyle(color: Colors.grey),
                                     ),
                                     Padding(
                                       padding: const EdgeInsets.all(8.0),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        children: <Widget>[
-                                          Text(
-                                            "5m",
-                                            style: TextStyle(color: Colors.grey),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Icon(
-                                              Icons.star_border,
-                                              size: 35.0,
-                                              color: Colors.grey,
-                                            ),
-                                          ),
-                                        ],
+                                      child: Icon(
+                                        Icons.star_border,
+                                        size: 35.0,
+                                        color: Colors.grey,
                                       ),
                                     ),
                                   ],
                                 ),
-                                Divider(
-                                  height: 2.0,
-                                  color: Colors.grey,
-                                )
-                              ],
-                            ),
-                          );
-                        }
-                      }),
-                )),
-          ].reversed.toList(),
+                              ),
+                            ],
+                          ),
+                          Divider(
+                            height: 2.0,
+                            color: Colors.grey,
+                          )
+                        ],
+                      );
+                    }
+                  }),
+            )),
+          ].reversed.toList(), //ForReverseOrder
         ));
   }
 }
